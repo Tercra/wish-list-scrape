ae16044b6636f8957a126a170ed5ac63d13211be
Vicky <nakamanoaki@gmail.com>
2023-04-13 02:38:29 -0400

    Wrote tests for requestURL and extractOrigin, also added a try except statement in requestURL

--- productInfo.py
+++ productInfo.py
@@ -4,14 +4,26 @@ import re
 
 def requestURL(url):
     header = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"}
-    req = requests.get(url, headers=header)
+    try:
+        req = requests.get(url, headers=header)
+    except:
+        print(f"Invalid url: {url}")
+    else:
+        return {"success" : True, "req":req}
 
-    return req
+    return {"success" : False, "req":None}
 
-def extractOrigin(url):
-    return re.search(r"^(https://)?(www\.)?(\S+?)\.(com|co\.jp)", url).group(3)
+def extractOrigin(url):     #Can return None if no match
+    m = re.search(r"^(https://)?(www\.)?(\S+?)\.(com|co\.jp)", url)
+
+    if m is None:
+        return "N/A"
+
+    return m.group(3)
 
 
 if __name__ == "__main__":
     print(requestURL("https://otakurepublic.com/product/product_page_5741091.html"))
     print(extractOrigin("https://otakurepublic.com/product/product_page_5741091.html"))
+
+    print(requestURL("https://ww.cdjapan.co.jp/"))
--- testScraping.py
+++ testScraping.py
@@ -1,7 +1,25 @@
 import unittest
+import productInfo
 
 class TestScrapingMethods(unittest.TestCase):
-    pass
+    def test_requestURL(self):
+        self.assertEqual(productInfo.requestURL("https://otakurepublic.com/product/product_page_5741091.html")["req"].status_code, 200)
 
+        self.assertEqual(productInfo.requestURL("https://ww.cdjapan.co.jp/")["success"], False)
+        self.assertEqual(productInfo.requestURL("https://store.crunchyroll.coco/")["success"], False)
+
+
+    def test_extractOrigin(self):
+        self.assertEqual(productInfo.extractOrigin("https://otakurepublic.com/product/product_page_5741091.html"), "otakurepublic")
+        self.assertEqual(productInfo.extractOrigin("https://www.cdjapan.co.jp/product/NEODAI-11875"), "cdjapan")
+        self.assertEqual(productInfo.extractOrigin("https://www.omocat-shop.com/collections/omocat-x-hololive-en/products/holomyth-long-sleeve-shirt"), "omocat-shop")
+        self.assertEqual(productInfo.extractOrigin("https://store.crunchyroll.com/products/blue-lock-bachira-meguru-pop-up-parade-4580416947268.html"), "store.crunchyroll")
+        self.assertEqual(productInfo.extractOrigin("https://www.amiami.com/eng/detail?scode=FIGURE-153159&rank="), "amiami")
+
+        self.assertNotEqual(productInfo.extractOrigin("htttttps://otakurepublic.com/product/product_page_5741091.html"), "otakurepublic")
+        self.assertEqual(productInfo.extractOrigin("https://otakurepublic.coom/"), "N/A")
+        self.assertNotEqual(productInfo.extractOrigin("htps:///w./www.etsy.com/"), "etsy")
+        
+        
 if __name__ == "__main__":
     unittest.main()
